08260e3f91456f0c02fda1a395f1511585a09620
kengchayodom <chayodom_j@cmu.ac.th>
2025-02-28 00:41:40 +0700

    change graphqlUri for backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://52.74.162.217:8085/graphql'
-
+  graphqlUri: 'http://54.89.143.49:8085/graphql'  // เปลี่ยน IP address ให้เป็นของเซิร์ฟเวอร์ของคุณ
 };
