9e36c02c890ef2d2e74957e6f7d757afd1100764
kengchayodom <chayodom_j@cmu.ac.th>
2025-02-28 10:36:55 +0700

    edit ja

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://54.89.143.49:8085/graphql'  // เปลี่ยน IP address ให้เป็นของเซิร์ฟเวอร์ของคุณ
+  graphqlUri: 'http://54.197.36.47:8085/graphql'  // เปลี่ยน IP address ให้เป็นของเซิร์ฟเวอร์ของคุณ
 };
